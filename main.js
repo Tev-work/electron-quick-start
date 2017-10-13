e88bd72a395db8af0ceaf0979f9db42694c8a1fb
Tev <tev@Tevs-iMac.local>
2017-10-13 19:53:35 +0200

    Loading IndexedDB example from MDN, see https://developer.mozilla.org/en-US/docs/Web/API/IndexedDB_API/Using_IndexedDB

--- main.js
+++ main.js
@@ -16,11 +16,7 @@ function createWindow () {
   mainWindow = new BrowserWindow({width: 800, height: 600})
 
   // and load the index.html of the app.
-  mainWindow.loadURL(url.format({
-    pathname: path.join(__dirname, 'index.html'),
-    protocol: 'file:',
-    slashes: true
-  }))
+  mainWindow.loadURL('https://mdn.mozillademos.org/en-US/docs/Web/API/IndexedDB_API/Using_IndexedDB$samples/Full_IndexedDB_example?revision=1317730')
 
   // Open the DevTools.
   // mainWindow.webContents.openDevTools()
